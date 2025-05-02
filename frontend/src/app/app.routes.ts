0374ae396ab1b07fb4c36ad4cb4d7a3598d1fae8
Aitor Sagarduy <aitor.sagarduy@opendeusto.es>
2025-05-02 23:19:06 +0200

    Mejorado el componente

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { MenuInicialComponent } from './menu-inicial/menu-inicial.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'INDITEXTECH', component:MenuInicialComponent, pathMatch: 'full'},
+    {path: '**', redirectTo: 'INDITEXTECH'}
+];
