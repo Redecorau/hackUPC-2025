8a39328d1b7c2b4f14fd185b3fab69cc3c2b73f0
r2213432 <jhojanmartinezmayta@gmail.com>
2025-05-02 23:38:59 +0200

    Creado servicio de camara Y esquema de datos para backend

---
+++ backend/BD/esquemas/esquema_imagen.py
@@ -0,0 +1,13 @@
+def entidad_esquema(entidad)-> dict : #Devuelve un diccionario a modo de json borja
+    return {
+        "id":str(entidad["_id"]),
+        "tipo" : entidad["tipo"],
+        "nombre" : entidad["nombre"],
+        "vida" : entidad["vida"],
+        "tmp_atac" : entidad["tmp_atac"],
+        "danho" : entidad["danho"],
+        "nivel": entidad["nivel"]
+    }
+
+def entidades_esquema(entidades)-> list: #Devuelve una lista de json de entidades
+    return [entidad_esquema(entidad) for entidad in entidades]
